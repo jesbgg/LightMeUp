56b388e34b0ee4870d771c56613771d45d4e7779
jesbgg <103523649@student.swin.edu.au>
2022-08-27 22:47:16 +1000

    View model, three images done

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -3,40 +3,73 @@ package com.example.myapplication
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ImageView
+import androidx.activity.viewModels
+import androidx.appcompat.content.res.AppCompatResources
+import androidx.lifecycle.ViewModel
 
 
 class MainActivity : AppCompatActivity() {
+
+    val imageViewModel : ImageViewModel by viewModels()
 
     companion object {
         private const val  KEY_IMAGE = "IMAGE"
     }
 
-    var love = R.drawable.ic_favorite_black_24dp
+//    var love = R.drawable.ic_favorite_black_24dp
+
+//    class MainActivity : AppCompatActivity() {
+//        private const val KEY_IMAGE = "IMAGE"
+//    }
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val imageView = findViewById<ImageView>(R.id.imageView)
+        imageView.setImageDrawable(AppCompatResources.getDrawable(this, imageViewModel.love))
 
-        savedInstanceState?.let{
-            love = it.getInt(KEY_IMAGE)
-            imageView.setImageDrawable(getDrawable(love))
-        }
+//        savedInstanceState?.let{
+//            love = it.getInt(KEY_IMAGE)
+//            imageView.setImageDrawable(getDrawable(love))
+//        }
 
         imageView.setOnClickListener {
-            love = when (love) {
-                R.drawable.ic_favorite_black_24dp -> R.drawable.ic_favorite_border_black_24dp
-                R.drawable.ic_favorite_border_black_24dp -> R.drawable.ic_favorite_black_24dp
-                else -> R.drawable.ic_favorite_black_24dp
-            }
-            imageView.setImageDrawable(getDrawable(love))
+            imageViewModel.nextImage()
+//            love = when (love) {
+//                R.drawable.ic_favorite_black_24dp -> R.drawable.ic_favorite_border_black_24dp
+//               R.drawable.ic_favorite_border_black_24dp -> R.drawable.ic_grade_black_24dp
+//                R.drawable.ic_grade_black_24dp -> R.drawable.ic_favorite_black_24dp
+
+//                else -> R.drawable.ic_favorite_black_24dp
+//            }
+//            imageView.setImageDrawable(getDrawable(love))
+            imageView.setImageDrawable(getDrawable(imageViewModel.love))
+            true
+          }
         }
 
     }
 
-    override fun onSaveInstanceState(outState: Bundle) {
-        super.onSaveInstanceState(outState)
-        outState.putInt(KEY_IMAGE, love)
+//    override fun onSaveInstanceState(outState: Bundle) {
+//        super.onSaveInstanceState(outState)
+//        outState.putInt(KEY_IMAGE, love)
+//    }
+//}
+
+class ImageViewModel: ViewModel() {
+
+    var love = R.drawable.ic_favorite_black_24dp
+
+    fun nextImage() {
+        love = when (love) {
+            R.drawable.ic_favorite_black_24dp -> R.drawable.ic_favorite_border_black_24dp
+            R.drawable.ic_favorite_border_black_24dp -> R.drawable.ic_grade_black_24dp
+            R.drawable.ic_grade_black_24dp -> R.drawable.ic_favorite_black_24dp
+            else -> R.drawable.ic_favorite_black_24dp
+        }
+
     }
+
 }
